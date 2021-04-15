0c95360a44de1ad9b7c6215498b232f75e2baf3e
Oscar Bergström <oscar@obstrom.com>
2021-04-15 13:13:57 +0200

    Searchbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import SearchResults from "./components/SearchResults";
 import StartInfo from "./components/StartInfo";
 // import ScannerBox from "./components/ScannerBox";
+import { createServer } from 'miragejs';
+
+createServer({
+    routes() {
+      this.get("/api/users", () => [
+        { id: "1", name: "Luke" },
+        { id: "2", name: "Leia" },
+        { id: "3", name: "Anakin" },
+      ])
+    },
+  })
 
 function App() {
     const [searchInput, setSearchInput] = useState("");
     const [showSearch, setShowSearch] = useState(false);
+    const [users, setUsers] = useState([]);
+
+    const searchInputElement = useRef(null);
 
     useEffect(() => {
+        if (!searchInput) {
+            setShowSearch(false);
+        }
+    });
+
+    function search() {
+        
+
+        let searchInput = searchInputElement.current.value;
+
+        console.log("click");
+        console.log(searchInput);
+
         if (searchInput) {
+            fetch("/api/users")
+                .then((response) => response.json())
+                .then((json) => setUsers(json));
             setShowSearch(true);
         } else {
             setShowSearch(false);
         }
-    });
+    }
+
+    function searchButtonClick() {
+        //setSearchInput(searchInputElement.current.value);
+        search();
+    }
 
     return (
         <>
@@ -24,10 +59,14 @@ function App() {
                     id="search-input"
                     placeholder="Sök produkt ..."
                     type="search"
+                    ref={searchInputElement}
                     onChange={(e) => setSearchInput(e.target.value)}
                 />
-                <button id="search-button">Sök</button>
+                <button id="search-button" onClick={searchButtonClick}>Sök</button>
             </div>
+            <ul>{users.map((user) => (
+        <li key={user.id}>{user.name}</li>
+      ))}</ul>
             {!showSearch && <StartInfo />}
             {showSearch && <SearchResults />}
         </div>
