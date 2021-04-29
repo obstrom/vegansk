d729f38ff560b77f9b1ac94e31ce67189be427ec
Cicci Carboo <cicci.carboo@outlook.com>
2021-04-29 10:46:28 +0200

    Textstyling

--- src/components/Product.js
+++ src/components/Product.js
@@ -14,53 +14,6 @@ function Product(props) {
   const productData = props.productDataAll[productId - 1];
   const specialIngredientsData = props.specialIngredients;
 
-  const CiccisKod = () => {
-    <>
-      <div>
-        <div className="Product container d-flex flex-column">
-          {/*Produktlandningssida här!*/}
-          <div></div>
-          <div className="product-top">
-            <div className="product-answer">
-              Ja{" "}
-              {/* Oscar: Ska HTML skrivas med gemener, som sedan justeras till versaler i CSS? Svar: Vanlig text, stor boxtav på första ordet*/}
-              <div className="specify-answer">Vegansk</div>
-            </div>
-          </div>
-          <div className="bottom">
-            <svg>{/* Bild på Florapaket */}</svg>
-            <div className="product-name">
-              Flora mjölkfri 400 gram {/* + SVG tummen upp? */}
-            </div>
-            <div className="product-information">
-              Produktinformation{/*Accordion?*/}
-            </div>
-            <div className="ingredients">Ingredienser</div>
-            <ul className="ingredient-list">
-              <li>Vatten</li>
-              <li>Palmolja</li>
-              <li>Rapsolja (24%)</li>
-              <li>Salt (1,2%)</li>
-              <li>Emulgeringsmedel</li>
-              <li>Naturlig arom</li>
-            </ul>
-            <div className="report-flawes">
-              <a className="flaw-link" href="">
-                Rapportera felaktighet
-              </a>
-            </div>
-            <br />
-          </div>
-          <footer className="footer">
-            <button className="new-search" onClick="blablabla()">
-              Gör ny sökning
-            </button>
-          </footer>
-        </div>
-      </div>
-    </>;
-  };
-
   const productTitleAwnser = (bool) => {
     if (bool) {
       return (
@@ -270,7 +223,7 @@ function Product(props) {
               {renderProductAllergens(productData.allergens.length)}
             </div>
             <div className="rating-container text-center">
-              <RatingWindow productId={productId}/>
+              <RatingWindow productId={productId} />
             </div>
             <div className="product-report-wrapper text-center">
               <a href="#">Rapportera felaktighet</a>
--- src/components/RatingWindow.js
+++ src/components/RatingWindow.js
@@ -1,79 +1,91 @@
-import {Modal, Button} from "react-bootstrap";
-import {useState} from "react";
+import { Modal, Button } from "react-bootstrap";
+import { useState } from "react";
 import StarRating from "./StarRating";
 import "./RatingWindow.css";
 import createServer from "./ProductList";
 
 export default function RatingWindow(props) {
-    const [show, setShow] = useState(false);
-    const [reviewCount, setReviewCount] = useState(0);
+  const [show, setShow] = useState(false);
+  const [reviewCount, setReviewCount] = useState(0);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-    const handleFormSubmit = (e) => {
-        e.preventDefault();
-    };
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+  };
 
-    const handleTextAreaCount = (e) => {
-        console.log(e);
-        const message = e.target.value;
-        const messageLength = message.length;
-        setReviewCount(messageLength);
-    };
+  const handleTextAreaCount = (e) => {
+    console.log(e);
+    const message = e.target.value;
+    const messageLength = message.length;
+    setReviewCount(messageLength);
+  };
 
-    return (
-        <>
-            <Button variant="primary" onClick={handleShow}>
-                Tyck till om produkten
-            </Button>
+  return (
+    <>
+      <Button variant="primary" onClick={handleShow}>
+        Tyck till om produkten
+      </Button>
 
-            <Modal className={"rating-modal"} show={show} onHide={handleClose}>
-
-                <Modal.Header className={"rating-modal-header"} closeButton>
-                    <Modal.Title>Hjälp andra genom att betygsätta produkten!</Modal.Title>
-                </Modal.Header>
-                <Modal.Body className={"rating-modal-body"}>
-                    <form className="rating-form">
-                        <div className="star-container">
-                        <label
-                            htmlFor={`product-id-${props.productId}`}
-                            className="rating-label"
-                        >
-                            <strong>Betygsätt produkten</strong> (obligatoriskt)
-                        </label>
-                        <StarRating id={props.productId}/>
-                        </div>
-                        <label htmlFor="product-review" className="review-label">
-                            <strong>Skriv en recension </strong>
-                            (ej obligatoriskt)
-                        </label>
-                        <textarea
-                            id="product-review"
-                            className="review-textarea"
-                            maxLength="150"
-                            name="review"
-                            onChange={handleTextAreaCount}
-                            placeholder="Din recension kommer att hjälpa andra människor att ta bättre beslut!"
-                        ></textarea>
-                        <span className="review-character-count">{`${reviewCount}/150`}</span>
-                        <label htmlFor="name-input" className="name-label">
-                            <strong>Ditt namn</strong>
-                            (obligatoriskt)
-                        </label>
-                        <input id="name-input" placeholder="Ditt namn..." name="name" className="review-input"></input>
-                        <p className="disclaimer">Dina svar hanteras anonymt, lämna inga personuppgifter. Vänligen kommentera endast på produkten. <br/>Tack!</p>
-                        <div>
-                            <Button variant="primary" onClick={handleFormSubmit}>
-                                Skicka betyg
-                            </Button>
-                            <Button variant="secondary" onClick={handleClose}>
-                                Avbryt
-                            </Button>
-                        </div>
-                    </form>
-                </Modal.Body>
-            </Modal>
-        </>
-    );
+      <Modal className={"rating-modal"} show={show} onHide={handleClose}>
+        <Modal.Header className={"rating-modal-header"} closeButton>
+          <Modal.Title>Hjälp andra genom att betygsätta produkten!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className={"rating-modal-body"}>
+          <form className="rating-form">
+            <div className="star-container">
+              <label
+                htmlFor={`product-id-${props.productId}`}
+                className="rating-label"
+              >
+                <strong>Betygsätt produkten</strong> (obligatoriskt)
+              </label>
+              <StarRating id={props.productId} />
+            </div>
+            <div className="review-container">
+              <label htmlFor="product-review" className="review-label">
+                <strong>Skriv en recension </strong>
+                (ej obligatoriskt)
+              </label>
+              <div className="review-wrapper">
+                <textarea
+                  id="product-review"
+                  className="review-textarea"
+                  maxLength="150"
+                  name="review"
+                  onChange={handleTextAreaCount}
+                  placeholder="Din recension kommer att hjälpa andra människor att ta bättre beslut!"
+                ></textarea>
+                <span className="review-character-count">{`${reviewCount}/150`}</span>
+              </div>
+              <label htmlFor="name-input" className="name-label">
+                <strong>Ditt namn</strong>
+                (obligatoriskt)
+              </label>
+              <input
+                id="name-input"
+                placeholder="Ditt namn..."
+                name="name"
+                className="review-input"
+              ></input>
+              <p className="disclaimer">
+                Dina svar hanteras anonymt, lämna inga personuppgifter. Vänligen
+                kommentera endast på produkten. <br />
+                Tack!
+              </p>
+            </div>
+            <div>
+              <Button variant="primary" onClick={handleFormSubmit}>
+                Skicka betyg
+              </Button>
+              <Button variant="secondary" onClick={handleClose}>
+                Avbryt
+              </Button>
+            </div>
+          </form>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
 }
