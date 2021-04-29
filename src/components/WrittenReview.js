43eeb0f60c75e540bcd049a036cc5c729f8d3cea
Oscar Bergström <oscar@obstrom.com>
2021-04-29 17:24:50 +0200

    Kundrecensioner

--- src/components/WrittenReview.js
+++ src/components/WrittenReview.js
@@ -1,8 +1,13 @@
 import StarRatingProductPage from "./StarRatingProductPage";
 
-export default function WrittenReview(){
-    return(<div className="single-review">
-        <StarRatingProductPage value={3}/>
+export default function WrittenReview(props) {
+    return (
+        <div className="single-review">
+            <StarRatingProductPage value={props.value} />
+            <p className="single-review-header">
+                <strong>{props.name}</strong>, {props.date}
+            </p>
+            <p className="single-review-text">{props.text}</p>
         </div>
-    )
+    );
 }
