5f6828851d3f772e53d976ac9818222486e37e2a
simon <anvedn@gmail.comn>
2021-05-19 14:21:45 +0200

    infobox done

--- src/components/ProductInfoBox.js
+++ src/components/ProductInfoBox.js
@@ -2,6 +2,33 @@ import React from 'react';
 import StarRatingProductPage from "./StarRatingProductPage";
 
 function ProductInfoBox(props) {
+
+
+    const decideBoxContent = () => {
+        if (props.productData.vegan) {
+            return (
+                <>
+                    Ett veganskt och njutbart alternativ till smör.
+                    <div className="product-info-rating">
+                        <StarRatingProductPage value={props.productAverageReview()}/>
+                    </div>
+                </>
+            )
+
+        } else {
+            return (
+                <div>
+                    <ul>
+                        <li><img src="/images/exclamation-mark.svg" alt="utropstecken"/><span>  Mjölk</span></li>
+                        <li><img src="/images/exclamation-mark.svg" alt="utropstecken"/><span>  Vassle</span></li>
+                        <li><img src="/images/exclamation-mark.svg" alt="utropstecken"/><span>  Animaliska E-ämnen</span></li>
+                    </ul>
+                </div>
+            )
+        }
+    }
+
+
     return (
         <div className="product-info-box-container">
             <div className="product-image-container">
@@ -14,11 +41,9 @@ function ProductInfoBox(props) {
                 </div>
             </div>
             <div className="product-info-text">
-                    <h3>{props.productData.name}</h3>
-                Ett bra smör helt enkelt! Ett bra smör helt enkelt! Ett bra smör helt enkelt! Ett bra smör helt enkelt!
-                <div className="product-info-rating">
-                    <StarRatingProductPage value={props.productAverageReview()}/>
-            </div>
+                <h3>{props.productData.name}</h3>
+                {decideBoxContent()}
+
             </div>
 
         </div>
