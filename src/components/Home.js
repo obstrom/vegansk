7a35ed18130f5c7e09c0722b623f2dae77b39382
Oscar Bergström <oscar@obstrom.com>
2021-05-25 11:09:09 +0200

    Added event listener for start bg image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,20 @@
 import StartSearch from "./StartSearch";
+import { useEffect } from "react";
 
 function Home(props) {
-    // Make body-tag (outside React) green for home page,
-    // and set lemon background image
+    // Make body-tag (outside React) green for home page
     document.body.style.backgroundColor = "#38514b";
-    document.body.style.backgroundImage = "url('./images/lemon_bg.png')";
+
+    // Handle start page background image on window resize
+    const windowWidthChange = () => {
+        if (document.body.clientWidth < 768) {
+            document.body.style.backgroundImage = "none";
+        } else {
+            document.body.style.backgroundImage =
+                "url('./images/lemon_bg.png')";
+        }
+    };
+    window.addEventListener("resize", windowWidthChange);
 
     return (
         <>
