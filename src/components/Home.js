7b166fd37c96fc416a2a581e543a16eec76b451b
Cicci Carboo <cicci.carboo@outlook.com>
2021-05-26 15:48:48 +0200

    product page navbar with content

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,39 +2,34 @@ import StartSearch from "./StartSearch";
 import { useEffect } from "react";
 
 function Home(props) {
-    // Make body-tag (outside React) green for home page
-    document.body.style.backgroundColor = "#38514b";
+  // Make body-tag (outside React) green for home page
+  document.body.style.backgroundColor = "#38514b";
 
-    // Handle start page background image on window resize
-    const windowWidthChange = () => {
-        if (document.body.clientWidth < 768) {
-            document.body.style.backgroundImage = "none";
-        } else {
-            document.body.style.backgroundImage =
-                "url('./images/lemon_bg.png')";
-        }
-    };
-    window.addEventListener("resize", windowWidthChange);
+  // Handle start page background image on window resize
+  const windowWidthChange = () => {
+    if (document.body.clientWidth < 768) {
+      document.body.style.backgroundImage = "none";
+    } else {
+      document.body.style.backgroundImage = "url('./images/lemon_bg.png')";
+    }
+  };
+  window.addEventListener("resize", windowWidthChange);
 
-    windowWidthChange()
+  windowWidthChange();
 
-    return (
-        <>
-            <div className="App container d-flex flex-column">
-                <div className="desktop-title">Vegansk.</div>
-                <div className="title-container">
-                    <img
-                        src="images/ica.png"
-                        className="title-icon"
-                        alt="ICA Logo"
-                    />
-                    <h1 className="title">Är den vegansk?</h1>
-                </div>
-                <StartSearch allProductsList={props.allProducts} />
-            </div>
-            {/*showSearch && <div class="sticky-fade"></div>*/}
-        </>
-    );
+  return (
+    <>
+      <div className="App container d-flex flex-column">
+        <div className="desktop-title">Vegansk.</div>
+        <div className="title-container">
+          <img src="images/ica.png" className="title-icon" alt="ICA Logo" />
+          <h1 className="title">Är den vegansk?</h1>
+        </div>
+        <StartSearch allProductsList={props.allProducts} compactMode={false} />
+      </div>
+      {/*showSearch && <div class="sticky-fade"></div>*/}
+    </>
+  );
 }
 
 export default Home;
